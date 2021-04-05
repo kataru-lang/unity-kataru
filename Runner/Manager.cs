9ccdb423015a95bc91aecdc707ad8c46c98aa50b
Angela He <heangela.arts@gmail.com>
2021-04-04 21:58:36 -0700

    Manager null check; update dylib

--- Runner/Manager.cs
+++ Runner/Manager.cs
@@ -29,8 +29,13 @@ namespace Kataru
 
         protected virtual void Awake()
         {
+#if UNITY_EDITOR
             if (initialized) Debug.LogError(@"A Kataru Manager was already initialized.
                 You should only have one Kataru Manager in your scene.");
+            if (string.IsNullOrEmpty(bookmarkPath)) Debug.LogError("bookmarkPath is null or empty.");
+            if (string.IsNullOrEmpty(storyPath)) Debug.LogError("storyPath is null or empty.");
+            if (string.IsNullOrEmpty(savePath)) Debug.LogError("savePath is null or empty.");
+#endif
             Runner.Init(
                 storyPath: absoluteStoryPath,
                 bookmarkPath: absoluteBookmarkPath,
