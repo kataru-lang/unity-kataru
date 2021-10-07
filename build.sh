5d84b620d8d81ce42b72797ac9d4e0bb8b2fb0aa
Angela He <heangela.arts@gmail.com>
2021-10-07 10:04:26 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 pushd .Rust
 cargo update
 cargo build --target x86_64-apple-darwin --all-features --release
+cargo build --target x86_64-unknown-linux-gnu --all-features --release
 popd
 
 cp .Rust/target/x86_64-apple-darwin/release/libkataru_ffi.dylib "Plugins/OSX/kataru_ffi.dylib"
+cp .Rust/target/x86_64-unknown-linux-gnu/release/libkataru_ffi.so "Plugins/Linux/kataru_ffi.so"
